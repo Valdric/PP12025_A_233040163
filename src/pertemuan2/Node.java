e90a93ffac03b3905f36718946130489fb6ac058
OldMan <146049953+Valdric@users.noreply.github.com>
2025-02-17 17:16:31 +0700

    menambahkan tugas 1 dan latihan

---
+++ src/pertemuan2/Node.java
@@ -0,0 +1,29 @@
+package pertemuan2;
+
+public class Node {
+
+	private int nilai;
+	private Node next;
+
+	// inisialisasi Node
+	public Node(int nilai) {
+		this.nilai = nilai;
+	}
+
+	// Setter & Getter
+	public void setNilai(int nilai) {
+		this.nilai = nilai;
+	}
+
+	public int getNilai() {
+		return nilai;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+}
