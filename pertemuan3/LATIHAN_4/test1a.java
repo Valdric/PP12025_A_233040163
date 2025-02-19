65d0e9a429f13e35f3ec12ec1fd54e0cb3fdc983
OldMan <146049953+Valdric@users.noreply.github.com>
2025-02-19 15:30:53 +0700

    Add files via upload

---
+++ pertemuan3/LATIHAN_4/test1a.java
@@ -0,0 +1,10 @@
+public class ListTest {
+public static void main(String[] args) {
+   StrukturList list = new StrukturList();
+   list.addTail(3);
+   list.addTail(2);
+   list.addTail(1);
+   list.displayList(); 
+ 
+}
+}
