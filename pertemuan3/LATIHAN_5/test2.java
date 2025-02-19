df01272e8a7fdd23fbb491c1c865b18d01e97feb
OldMan <146049953+Valdric@users.noreply.github.com>
2025-02-19 15:30:16 +0700

    Add files via upload

---
+++ pertemuan3/LATIHAN_5/test2.java
@@ -0,0 +1,10 @@
+public class ListTest {
+public static void main(String[] args) {
+   StrukturList list = new StrukturList();
+   list.addHead(5);
+   list.addHead(3);
+   list.addHead(4);
+   list.displayList(); 
+ 
+}
+}
---
+++ pertemuan3/LATIHAN_5/test3a.java
@@ -0,0 +1,10 @@
+public class ListTest {
+public static void main(String[] args) {
+   StrukturList list = new StrukturList();
+   list.addHead(1);
+   list.addHead(2);
+   list.addHead(3);
+   list.displayList(); 
+ 
+}
+}
